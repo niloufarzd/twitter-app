b470b3e0ffceffc6f87d56d15278c3fe19c2c459
niloufar <zaferaniniloufar@gmail.com>
2021-09-25 10:04:31 +0330

    create component layout

---
+++ src/Component/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export const Layout = () => {
+    return (
+        <div>
+            <div>right sidebar</div>
+            <div>left sidebar</div>
+            <div>main side bar</div>
+
+        </div>
+    )
+}
